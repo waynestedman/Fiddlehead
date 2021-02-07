aacd79955e89f44ee4c2529923c6f6f00316ef00
Wayne Stedman <waynestedman@users.noreply.github.com>
2021-02-06 18:40:39 -0800

    chore: backup of vue only site

---
+++ _support-files/vue-src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="mailto:someone@yoursite.com?subject=A Request from Our Visitor"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Contact Us</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HomeContent/>
+    </v-main>
+    <v-footer
+     color="deep-orange lighten-4"
+     padless
+    >
+      <Footer/>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import HomeContent from './components/HomeContent';
+import Footer from './components/Footer';
+
+export default {
+  name: 'App',
+
+  components: {
+    HomeContent,
+    Footer,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
