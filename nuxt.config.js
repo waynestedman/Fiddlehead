ef688ed4fb995c2316d98dee87b3e2853a5f7234
Wayne Stedman <waynestedman@users.noreply.github.com>
2021-03-01 11:33:32 -0800

    build: removed nav drawers; componentized footer

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,7 +50,7 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ['~/assets/sass/variables.scss'],
     theme: {
       dark: false,
       themes: {
