e2b70a5b40ce259c6d41fe7fd355a2015d7ce65f
Wayne Stedman <waynestedman@users.noreply.github.com>
2021-03-01 16:11:00 -0800

    build: added top nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,33 @@
 <template>
   <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+
+      <v-spacer />
+
+      <v-btn
+        href="mailto:someone@yoursite.com?subject=A Request from Our Visitor"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Contact Us</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
     <v-main>
       <v-container>
         <nuxt />
